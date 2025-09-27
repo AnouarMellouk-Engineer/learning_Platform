cf61a58726fa15ce7552375a06bb6efee33e1fe4
anouar mellouk <anouarmellouk.info@gmail.com>
2025-09-27 14:44:20 -0400

    add form routes

--- backend/src/modules/order/route.ts
+++ backend/src/modules/order/route.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { addOrder, getOrders, updateOrder } from "./controller";
+import {
+  addOrder,
+  getOrders,
+  updateOrder,
+  getForms,
+  addForm,
+} from "./controller";
 
 const router = Router();
 
 router.route("/").get(getOrders).post(addOrder).put(updateOrder);
+router.route("/form").get(getForms).post(addForm);
 
 export default router;

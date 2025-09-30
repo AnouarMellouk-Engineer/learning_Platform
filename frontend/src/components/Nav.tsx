1552db47c165c997c6c8e198edd150fa5fda0495
anouar mellouk <anouarmellouk.info@gmail.com>
2025-09-30 17:34:55 -0400

    create responsive navbar

---
+++ frontend/src/components/Nav.tsx
@@ -0,0 +1,49 @@
+import { Menu } from "lucide-react";
+import { X } from "lucide-react";
+import { useState } from "react";
+const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className=" phone-container md:px-0 py-3.5  bg-background-secondery w-full fixed top-0  ">
+      <div className="  md:container  mx-auto font-heading flex items-center justify-between">
+        <h1 className="text-3xl font-medium cursor-pointer flex-1">Elearing</h1>
+        <div
+          className={`flex flex-col md:flex-row justfiy-center md:justify-between gap-24 md:gap0 items-center  absolute top-0 h-[100vh] md:h-auto  w-1/2 md:w-auto ${
+            isOpen ? "right-0" : "right-[-1000px]"
+          } md:right-0 md:relative duration-500  flex-2 bg-white md:bg-transparent pt-32 md:pt-0 `}
+        >
+          <X
+            className="md:hidden  absolute top-6 right-7 cursor-pointer "
+            size={20}
+            onClick={() => {
+              setIsOpen(false);
+            }}
+          />
+
+          <ul className="  flex  flex-col  md:flex-row items-center justify-center gap-8">
+            {["About", "Courses", "Tracks", "Contact"].map((link) => (
+              <li
+                key={link}
+                className="text-md cursor-pointer duration-300 hover:text-primary"
+              >
+                {link}
+              </li>
+            ))}
+          </ul>
+          <button className="block px-8 py-3 bg-primary duration-300  rounded-full text-white text-lg font-text cursor-pointer">
+            Register
+          </button>
+        </div>
+
+        <Menu
+          className=" md:hidden cursor-pointer"
+          onClick={() => {
+            setIsOpen(true);
+          }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+  return (
+    <div className="h-[1000vh]">
+      <Nav />
+      <h2 className="mt-20">hello laning page</h2>
+    </div>
+  );
+};
+
+export default Home;

6a0d15ec34bbfc017a8ab748130c484cd167acf1
anouar mellouk <anouarmellouk.info@gmail.com>
2025-10-02 08:04:32 -0400

    update navbar (add shadcn components)

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,16 +1,19 @@
 import { Menu } from "lucide-react";
 import { X } from "lucide-react";
 import { useState } from "react";
+import { Button } from "./ui/button";
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className=" phone-container md:px-0 py-3.5  bg-background-secondery w-full fixed top-0  ">
+    <nav className=" phone-container md:px-0 py-3.5  bg-my-background-secondary  w-full fixed top-0   ">
       <div className="  md:container  mx-auto font-heading flex items-center justify-between">
-        <h1 className="text-3xl font-medium cursor-pointer flex-1">Elearing</h1>
+        <h1 className="font-my-heading text-3xl font-medium cursor-pointer flex-1">
+          Elearing
+        </h1>
         <div
           className={`flex flex-col md:flex-row justfiy-center md:justify-between gap-24 md:gap0 items-center  absolute top-0 h-[100vh] md:h-auto  w-1/2 md:w-auto ${
             isOpen ? "right-0" : "right-[-1000px]"
-          } md:right-0 md:relative duration-500  flex-2 bg-white md:bg-transparent pt-32 md:pt-0 `}
+          } md:right-0  md:relative duration-500  flex-2 bg-white md:bg-transparent pt-32 md:pt-0 `}
         >
           <X
             className="md:hidden  absolute top-6 right-7 cursor-pointer "
@@ -24,23 +27,23 @@ const Nav = () => {
             {["About", "Courses", "Tracks", "Contact"].map((link) => (
               <li
                 key={link}
-                className="text-md cursor-pointer duration-300 hover:text-primary"
+                className="text-md cursor-pointer duration-300 hover:text-my-primary"
               >
                 {link}
               </li>
             ))}
           </ul>
-          <button className="block px-8 py-3 bg-primary duration-300  rounded-full text-white text-lg font-text cursor-pointer">
-            Register
-          </button>
+          <Button className="px-8 py-6">Register</Button>
         </div>
-
-        <Menu
-          className=" md:hidden cursor-pointer"
+        <div
+          className=" md:hidden cursor-pointer flex items-center gap-1.5 font-my-heading"
           onClick={() => {
             setIsOpen(true);
           }}
-        />
+        >
+          <p>Menu</p>
+          <Menu />
+        </div>
       </div>
     </nav>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Nav from "../components/Nav";
 
 const Home = () => {
   return (
     <div className="h-[1000vh]">
       <Nav />
-      <h2 className="mt-20">hello laning page</h2>
+      <h2 className="mt-20">
+        hello laning page
+        <Button>register 2</Button>
+      </h2>
     </div>
   );
 };

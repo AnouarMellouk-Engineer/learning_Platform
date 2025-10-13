b47458cc815d82e30dbba8af2b778ab959afebc0
anouar mellouk <anouarmellouk.info@gmail.com>
2025-10-13 11:54:46 -0400

    complete the about us page design

--- frontend/src/components/SocialIcon.tsx
+++ frontend/src/components/SocialIcon.tsx
@@ -1,6 +1,6 @@
 const SocialIcon = ({ src }: { src: string }) => {
   return (
-    <div className="p-2 rounded-full border-1 border-black overflow-hidden w-9">
+    <div className="p-2 rounded-full border-1 border-black/30 duration-300 hover:bg-my-background overflow-hidden w-9">
       <img src={src} alt="" className="w-full" />
     </div>
   );
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -18,23 +18,24 @@ import Heading from "@/components/Heading";
 import Instructor from "@/components/Instructor";
 import StepSection from "@/components/StepSection";
 import { Button } from "@/components/ui/button";
+import ExplorSectioon from "@/layouts/ExplorSectioon";
 
 // export default About;
 
 const About = () => {
   return (
     <>
-      <div
+      {/* <div
         className="relative min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center pt-36"
         style={{
           backgroundImage:
             "url('./src/assets/images/redmind-studio-OLXBoNfb8kA-unsplash.jpg')",
         }}
       >
-        {/* Overlay */}
+       
         <div className="absolute inset-0 bg-black/60" />
 
-        {/* Content */}
+      
         <div className="relative z-10 max-w-3xl text-center px-6 md:px-12">
           <h1 className="text-4xl md:text-6xl font-bold text-white mb-6">
             Who We Are
@@ -50,7 +51,32 @@ const About = () => {
 
           <Button> Get Started</Button>
         </div>
-      </div>
+      </div>  */}
+
+      <ExplorSectioon className="gap-16 lg:gap-0" reverse={true}>
+        <div className="flex-1 flex lg:block flex-col items-center justify-start">
+          <h1 className="text-3xl  text-center lg:text-left md:text-3xl  font-bold  mb-6">
+            Who We Are
+          </h1>
+          <p className="text-lg text-center lg:text-left md:text-md leading-relaxed mb-12">
+            Welcome to <span className="font-semibold ">Elearning</span>, an
+            online learning space built to make education simple, accessible,
+            and engaging. Whether you are a student, a professional, or a
+            lifelong learner, we are here to help you grow your skills and
+            achieve your goals.
+          </p>
+
+          <Button className="px-8 py-6"> Get Started</Button>
+        </div>
+
+        <div className=" w-full lg:w-1/2 overflow-hidden flex-1">
+          <img
+            src="./src/assets/images/About us page-pana.svg"
+            alt=""
+            className=" w-[80%] lg:w-[100%] ml-12"
+          />
+        </div>
+      </ExplorSectioon>
       <div className="my-container">
         <Heading>
           {" "}
@@ -86,15 +112,33 @@ const About = () => {
           <StepSection />
         </div>
       </div>
-      <div className=" my-container  bg-my-secondary py-8 mb-10 ">
+      <div className=" my-container  bg-my-background py-8 my-10 ">
         <Heading>
           {" "}
           <h2 className="text-4xl font-my-heading mb-4 font-bold text-center">
-            Steps to Get
-            <span className="text-my-primary font-bold"> Started</span>
+            <span className="text-my-primary font-bold"> Team </span>
+            Members
           </h2>
         </Heading>
         <div className="flex justify-center gap-14 items-center flex-wrap">
+          <Instructor />
+          <Instructor />
+          <Instructor />
+        </div>
+      </div>
+
+      <div className=" my-container   py-8 my-10 ">
+        <Heading>
+          {" "}
+          <h2 className="text-4xl font-my-heading mb-4 font-bold text-center">
+            <span className="text-my-primary font-bold"> Team </span>
+            Members
+          </h2>
+        </Heading>
+        <div className="flex justify-center gap-14 items-center flex-wrap">
+          <Instructor />
+          <Instructor />
+          <Instructor />
           <Instructor />
           <Instructor />
           <Instructor />
